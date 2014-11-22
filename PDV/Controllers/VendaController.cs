a6358fa2812f4a3700e373b28b8212ecb65e065d
viniborges <viniborges@yahoo.com.br>
2014-11-22 03:30:49 -0200

    Primeiro teste com form tipo master-detail para registrar a venda/itensvenda

--- PDV/Controllers/VendaController.cs
+++ PDV/Controllers/VendaController.cs
@@ -48,52 +48,93 @@ namespace PDV.Controllers
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include="VendaId,ClienteId,DataMovimento,UsuarioId,ValorTotalVenda")] Venda venda)
+        public JsonResult Create(Venda venda)
         {
-            if (ModelState.IsValid)
+            try
             {
-                db.Vendas.Add(venda);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+                //if (ModelState.IsValid)
+                //{
+                    //Caso o VendaId seja maior que 0, significa que é uma edição de registro já existente
+                    if (venda.VendaId > 0)
+                    {
+                        var VendaAtual = db.VendaItens.Where(p => p.VendaId == venda.VendaId);
+                        foreach (VendaItem vi in VendaAtual)
+                            db.VendaItens.Remove(vi);
 
-            ViewBag.UsuarioId = new SelectList(db.Usuarios, "Id", "Email", venda.UsuarioId);
-            return View(venda);
+                        foreach (VendaItem vi in venda.VendaItem)
+                            db.VendaItens.Add(vi);
+
+                        db.Entry(venda).State = EntityState.Modified;
+                    }
+                    else
+                    {
+                        db.Vendas.Add(venda);
+                    }
+                    db.SaveChanges();
+                    return Json(new {Success = 1, VendaID = venda.VendaId, ex = ""});
+                //}
+            }
+            catch (Exception ex)
+            {
+                return Json(new { Success = 0, ex = ex.Message.ToString() });
+            }
+            return Json(new {Success = 0, ex = new Exception("Ops, ocorreu um erro ao salvar").Message.ToString()});
+
         }
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create([Bind(Include="VendaId,ClienteId,DataMovimento,UsuarioId,ValorTotalVenda")] Venda venda)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Vendas.Add(venda);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    ViewBag.UsuarioId = new SelectList(db.Usuarios, "Id", "Email", venda.UsuarioId);
+        //    return View(venda);
+        //}
 
         // GET: /Venda/Edit/5
-        public ActionResult Edit(int? id)
+        public ActionResult Edit(int id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
+            ViewBag.Title = "Editar";
             Venda venda = db.Vendas.Find(id);
-            if (venda == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.UsuarioId = new SelectList(db.Usuarios, "Id", "Email", venda.UsuarioId);
-            return View(venda);
+
+            //Call Create View
+            return View("Create", venda);
         }
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Venda venda = db.Vendas.Find(id);
+        //    if (venda == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    ViewBag.UsuarioId = new SelectList(db.Usuarios, "Id", "Email", venda.UsuarioId);
+        //    return View(venda);
+        //}
 
         // POST: /Venda/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include="VendaId,ClienteId,DataMovimento,UsuarioId,ValorTotalVenda")] Venda venda)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(venda).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.UsuarioId = new SelectList(db.Usuarios, "Id", "Email", venda.UsuarioId);
-            return View(venda);
-        }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include="VendaId,ClienteId,DataMovimento,UsuarioId,ValorTotalVenda")] Venda venda)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(venda).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    ViewBag.UsuarioId = new SelectList(db.Usuarios, "Id", "Email", venda.UsuarioId);
+        //    return View(venda);
+        //}
 
         // GET: /Venda/Delete/5
         public ActionResult Delete(int? id)
--- PDV/Models/Venda.cs
+++ PDV/Models/Venda.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -12,15 +13,19 @@ namespace PDV.Models
         //    VendaItens = new List<VendaItem>();
         //}
         public int VendaId { get; set; }
+
+        [Display(Name = "Cliente")]
         public int ClienteId { get; set; }
 
         public virtual Cliente Cliente { get; set; }
-
+        
+        [Display(Name = "Data da Venda")]
+        [DataType(DataType.DateTime)]
         public DateTime DataMovimento { get; set; }
         public int UsuarioId { get; set; }
         public virtual Usuario Usuario { get; set; }
         public double ValorTotalVenda { get; set; }
         public virtual IList<VendaItem> VendaItem { get; set; }
-        
+
     }
 }
